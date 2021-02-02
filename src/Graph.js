8dbee5fc351e654e85354a91db3a7656ea627796
Kevin Botero <kbotero201@gmail.com>
2021-02-01 19:01:51 -0500

    audio graph

---
+++ src/Graph.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+import Chart from "chart.js";
+
+
+ function Graph() {
+
+    useEffect(() => {
+      const ctx = document.getElementById("myChart")
+      //const xlabels = 
+      new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['', '250', '500', '1,000', '2,000', '4,000', '8,000', ''],
+            datasets: [{
+                label: 'Hearing Level',
+                lineTension: 0,
+                fill: false,
+                pointRadius: 10,
+                data: [null, 40, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                
+                xAxes: [{
+                    position: 'top',
+                    
+                
+                }],
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        max: 80,
+                        stepSize: 10,
+                        reverse: true,
+                        //beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    })
+
+
+
+    return (
+      <div>
+        <canvas id="myChart" width="100" height="100" />
+      </div>
+    )
+
+
+  }
+
+
+
+  export default Graph
---
+++ src/Test.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import Chart from 'chart.js';
+
+
+
+
+function Test(){
+
+const ctx = document.getElementById("myChart");
+
+    return(
+        <div>
+
+            <p> Test text</p>
+
+            <tr>
+          <td>Button</td>
+          <td>Button</td>
+          <td>Button</td>
+          <td>Button</td>
+          <td>Button</td>
+          <td>Button</td>
+          <td>Button</td>
+          <td>Button</td>
+          <td>Button</td>
+          <td>Button</td>
+          <td>Button</td>
+          <td>Button</td>
+      </tr>
+
+      <button>Back</button>
+      <button>Next</button>
+
+      
+        </div>
+    )
+}
+
+export default Test
